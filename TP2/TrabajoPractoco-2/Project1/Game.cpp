39a07f2c5cb6e4ca4329ecb9ee67c68a4013f366
Unknown <theoleyenda1997@gmail.com>
2018-04-30 17:31:21 -0300

    Dispara una bala

--- TP2/TrabajoPractoco-2/Project1/Game.cpp
+++ TP2/TrabajoPractoco-2/Project1/Game.cpp
@@ -1,11 +1,11 @@
 /*
-public:
+public
 Game();
 ~Game();
-void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+void init(const char title, int xpos, int ypos, int width, int height, bool fullscreen);
 void update();
-void render();//renderiza
-void clean();//limpia la pantalla
+void render(); renderiza
+void clean(); limpia la pantalla
 void handlEvents();
 bool running();
 };
@@ -22,29 +22,34 @@ Game::~Game()
 void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	int flags = 0;
+	bool balaAvanza = false;
 	if (fullscreen)
 	{
 		flags = SDL_WINDOW_FULLSCREEN;
 	}
-	
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
 		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-		if (window) 
+		if (window)
 		{
-			cout << "la ventana se ha creado" << endl;
+			cout<<"la ventana se ha creado"<<endl;
 		}
 
 		renderer = SDL_CreateRenderer(window, -1, 0);
 		if (renderer)
 		{
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			cout << "el renderer se ha creado" << endl;
+			cout<<"el renderer se ha creado"<<endl;
 		}
 		isRunning = true;
-		tmpSurface = IMG_Load("../thumbnail_Avion_2.png");
+		tmpSurface = IMG_Load("../thumbnail_Avion_1.png");
 		texturePlayer = SDL_CreateTextureFromSurface(renderer, tmpSurface);
 		SDL_FreeSurface(tmpSurface);
+
+		tmpSurfaceBala = IMG_Load("../bala1.png");
+		textureBala = SDL_CreateTextureFromSurface(renderer, tmpSurfaceBala);
+		SDL_FreeSurface(tmpSurfaceBala);
 	}
 	else
 	{
@@ -55,11 +60,23 @@ void Game::update()
 {
 	destRJugador.h = 64;
 	destRJugador.w = 64;
+	destRBala.h = 7;
+	destRBala.w = 3;
+	if (!balaAvanza)
+	{
+		destRBala.x = destRJugador.x + 30;
+		destRBala.y = destRJugador.y - 5;
+	}
+	if (balaAvanza)
+	{
+		destRBala.y = destRBala.y - 1;
+	}
 }
-void Game::render() 
+void Game::render()
 {
 	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer, texturePlayer, NULL, &destRJugador);
+	SDL_RenderCopy(renderer, textureBala, NULL, &destRBala);
 	SDL_RenderPresent(renderer);
 }
 void Game::clean()
@@ -67,34 +84,36 @@ void Game::clean()
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
-	cout << "Juego limpiado" << endl;
+	cout<<"Juegolimpiado"<<endl;
 }
-void Game::handlEvent() //input
-{
+void Game::handlEvent() {
 	SDL_Event event;
 	SDL_PollEvent(&event);
 	switch (event.key.keysym.sym)
 	{
-		case SDLK_ESCAPE:
-			isRunning = false;
+			case SDLK_SPACE:
+				balaAvanza = true;
 			break;
-		case SDLK_DOWN:
-			destRJugador.y = destRJugador.y + 10;
-			break;
-		case SDLK_UP:
-			destRJugador.y = destRJugador.y - 10;
-			break;
-		case SDLK_LEFT:
-			destRJugador.x = destRJugador.x - 10;
-			break;
-		case SDLK_RIGHT:
-			destRJugador.x = destRJugador.x + 10;
-			break;
-		default:
+			case SDLK_ESCAPE:
+				isRunning = false;
+				break;
+			case SDLK_DOWN:
+				destRJugador.y = destRJugador.y + 10;
+				break;
+			case SDLK_UP:
+				destRJugador.y = destRJugador.y - 10;
+				break;
+			case SDLK_LEFT:
+				destRJugador.x = destRJugador.x - 10;
+				break;
+			case SDLK_RIGHT:
+				destRJugador.x = destRJugador.x + 10;
+				break;
+			default:
 				break;
 	}
 }
-bool Game::running() 
+bool Game::running()
 {
 	return isRunning;
 }
--- TP2/TrabajoPractoco-2/Project1/Game.h
+++ TP2/TrabajoPractoco-2/Project1/Game.h
@@ -11,13 +11,20 @@ using namespace std;
 class Game
 {
 private:
+	bool balaAvanza;
 	bool isRunning;
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+
 	SDL_Texture* texturePlayer;
 	SDL_Surface* tmpSurface;
 	SDL_Rect srcRJugador;
 	SDL_Rect destRJugador;
+
+	SDL_Texture* textureBala;
+	SDL_Surface* tmpSurfaceBala;
+	SDL_Rect srcRBala;
+	SDL_Rect destRBala;
 public:
 	Game();
 	~Game();
@@ -26,7 +33,7 @@ public:
 	void render();//renderiza
 	void clean();//limpia la pantalla
 	void handlEvent(); //input
-	
+
 	bool running();
 };
 #endif
